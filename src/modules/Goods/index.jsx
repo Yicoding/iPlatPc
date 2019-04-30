2c5cf6fbcca05cb61c96f67596a795d83333dd7e
Yicoding <18063229116@@163.com>
2019-04-30 16:09:37 +0800

    add

--- src/modules/Goods/index.jsx
+++ src/modules/Goods/index.jsx
@@ -4,12 +4,30 @@ export default class Home extends Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      books: [1, 2, 3]
     }
+  }
+  _renderBook = () => {
+    let { books } = this.state
+    return books.map((item, index) => {
+      return (
+        <li key={index}>{index}</li>
+      )
+    })
+  }
+  add = () => {
+    let { books } = this.state
+    books.push(4)
+    this.setState({ books })
   }
   render() {
     return (
-      <div>goods</div>
+      <div>
+        <ul>
+          {this._renderBook()}
+          <button onClick={this.add}>add</button>
+        </ul>
+      </div>
     )
   }
 }
