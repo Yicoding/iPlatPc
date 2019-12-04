f380e8592c9c34ffcb30887d5cabb51c81441f82
Yicoding <18063229116@163.com>
2019-12-04 09:19:34 +0800

    打印完成

--- server/print.js
+++ server/print.js
@@ -54,45 +54,31 @@ app.post('/printOrderById', async function (req, res) {
     //   tokenData.machineCode = result.body.machine_code;
     // }
     const tokenData = {
-      accessToken: '37ba5669eb46450db3614d707b768a2f',
-      refreshToken: '11fa636b37ab45ed8da83f1e860a8289'
+      accessToken: 'e4b1ffb0f05a4fe885798922d4823c00',
+      refreshToken: '4d3cde29cf00459a922aca0efe423187'
     }
     var RpcClient = new yly.RpcClient(tokenData.accessToken, config);
     var Print = new yly.Print(RpcClient);
-    const content = `
-      <FS2><center>**#恒祥茶庄**</center></FS2>
-      ${'.'.repeat(32)}
-      <FS2><center>--在线支付--</center></FS2>
-      订单时间:${changeDate(new Date(), 'yyyy-MM-dd HH:mm')}\n
-      订单编号:${data.data.id}\n
-      ${'*'.repeat(32)}商品${'*'.repeat(32)}
-      <table style="color: #333; font-size: 14px; width: 100%;">
-        <tr>
-          <td>商品名</td>
-          <td>单价</td>
-          <td>数量</td>
-          <td>合计</td>
-        </tr>
-        ${
-      goodList.map(item => {
-        return `
-              <tr>
-                <td>${item.name}</td>
-                <td>${item.sale}</td>
-                <td>x${item.num}${item.unitType == 1 ? item.unitSingle : item.unitAll}</td>
-                <td>${item.total}</td>
-              </tr>
-            `
-      }).join('')
-      }
-      </table>
-      ${'.'.repeat(32)}
-      <QR>这是二维码内容</QR>
-      ${'.'.repeat(32)}
-      订单总价: ¥ ${data.data.total} 元\n
-      <FS2><center>**#end**</center></FS2>
-    `;
+    var str = goodList.map(item => {
+        return `<FS>${item.name} ${item.sale} x${item.num}${item.unitType == 1 ? item.unitSingle : item.unitAll} ${item.total}</FS>\n`
+      }).join('');
+    var content = `<FS2>**#恒祥茶庄**</FS2>\n`;
+    content += `${'.'.repeat(40)}\n`;
+    content += `订单时间:${changeDate(new Date(), 'yyyy-MM-dd HH:mm')}\n`;
+    content += `订单编号:${data.data.id}\n`;
+    content += `${'*'.repeat(20)}商品${'*'.repeat(20)}\n`;
+    content += `<table>`;
+    content += `<tr><td>商品名</td><td>单价</td><td>  数量</td><td>合计(元)</td></tr>`;
+    for (let i = 0; i < goodList.length; i ++) {
+    	const item = goodList[i];
+    	content += `<tr><td>${item.name}</td><td>${item.sale}</td><td>x${item.num}${item.unitType == 1 ? item.unitSingle : item.unitAll}</td><td>${item.total}</td></tr>`;
+    }
+    content += `</table>`;
+    content += `${'.'.repeat(40)}\n`;
+    content += `订单总价: ¥ ${data.data.total} 元\n`;
+    content += `<FS2>**#end**</FS2>`;
     // return console.log('content', content)
+    // await Print.cancelall('4004632435', 'orderNo1');
     await Print.index('4004632435', 'orderNo1', content);
     res.status(200).send({
       code: 0,
