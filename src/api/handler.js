a88aac652c89359505720155c30706eaf922a818
Yicoding <18063229116@163.com>
2020-01-16 16:21:31 +0800

    登录

--- src/api/handler.js
+++ src/api/handler.js
@@ -2,23 +2,44 @@
  * Promise handler
  */
 import axios from 'axios';
-
+import Store from '../common/js/storage';
+import { message } from 'antd';
 export function promiseHandler(options) {
   return new Promise((resolve, reject) => {
     // if (options.method === 'POST' || options.method === 'PUT') {
-    //   options.data = JSON.stringify(options.data)
-    // }
+      //   options.data = JSON.stringify(options.data)
+      // }
+    let phone = '';
+    let token = '';
+    const userInfoStore = Store.userInfoStore.get()
+    if (userInfoStore) {
+      phone = userInfoStore.value.phone;
+      token = userInfoStore.value.token;
+    }
     let requestBody = {
       method: options.method,
       url: options.url,
-      headers: {'Content-Type': 'application/json'}
+      headers: {
+        'Content-Type': 'application/json',
+        phone,
+        token
+      }
     }
     let key = (options.method === 'POST' || options.method === 'PUT') ? 'data' : 'params'
     requestBody[key] = options.data
     axios(requestBody).then(response => {
       let { status, data } = response;
       if (status === 200) {
-        return resolve(data);
+        if (data.code === 0) {
+          return resolve(data);
+        }
+        if (data.code === 403) {
+          console.log('登录过期');
+          message.warning('登录过期');
+          window.localStorage.clear();
+          window.location.href = window.location.origin + window.location.pathname + '#' + '/login';
+          return reject(data);
+        }
       }
       console.error(`[api status error]: ${status}`);
       reject(`[api status error]: ${status}`);
